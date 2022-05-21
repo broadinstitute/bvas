180ff75de25c33f6e30ecd7024016e269e6676eb
martin jankowiak <jankowiak@gmail.com>
2022-05-20 17:10:07 -0700

    add additional test for allele-level prior inclusion probabilities

--- tests/test_bvas_selector.py
+++ tests/test_bvas_selector.py
@@ -27,8 +27,10 @@ def test_bvas_selector(device, A=500, T=2000, T_burnin=200, report_frequency=500
 
     mutations = ["mut{}".format(k) for k in range(A)]
 
+    S = torch.ones(A).type_as(Y) / 500.0
+    S[2] = 0.9999  # strong a priori preference for allele 2
     selector = BVASSelector(Y, Gamma, mutations=mutations,
-                            S=(0.1, 100.0), tau=10.0,
+                            S=S, tau=10.0,
                             genotype_matrix=genotype_matrix, variant_names=variant_names)
 
     selector.run(T=T, T_burnin=T_burnin, seed=seed)
@@ -37,8 +39,8 @@ def test_bvas_selector(device, A=500, T=2000, T_burnin=200, report_frequency=500
     print(selector.summary.iloc[:7])
     assert selector.growth_rates.values.shape[0] == len(variant_names)
 
-    assert_close(summary['PIP'][:2].values, np.ones(2), atol=0.1)
-    assert summary['PIP'].values[2:].max().item() < 0.01
+    assert_close(summary['PIP'][:3].values, np.ones(3), atol=0.1)
+    assert summary['PIP'].values[3:].max().item() < 0.02
 
     assert_close(summary['Beta'].values[:2], np.array([beta0, beta1]), atol=0.1)
-    assert np.fabs(summary['Beta'].values[2:]).max().item() < 1.0e-4
+    assert np.fabs(summary['Beta'].values[3:]).max().item() < 1.0e-4
